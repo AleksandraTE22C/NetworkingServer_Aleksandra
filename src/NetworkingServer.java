f19b3f3758049b0ab9dc8a0bd7a74ba11694f42a
AleksandraTE22C <aleksandra.kajak-tonkiel@elev.ga.ntig.se>
2025-05-26 14:23:03 +0200

    Början på nätverkslabben

---
+++ src/NetworkingServer.java
@@ -0,0 +1,37 @@
+import java.net.*;
+import java.io.*;
+
+public class NetworkingServer {
+    public static void main(String[] args) {
+            ServerSocket server = null;
+            Socket client;
+
+            //Default port number we are going to use
+            int portnumber = 1234;
+            if(args.length >= 1){
+                portnumber = Integer.parseInt(args[0]);
+            }
+        //Create server side socket
+        try{
+            server = new ServerSocket(portnumber);
+        } catch (IOException ie) {
+            System.out.println("Cannot open socket." + ie);
+            System.exit(1);
+        }
+        System.out.println("ServerSocket is created." + server);
+
+        //Wait for the data from the client and reply
+        while(true){
+            try{
+                //Listens for a connection to be made to
+                //this socket and accepts it. The method blocks until
+                //a connection is made
+                System.out.println("Waiting for connect request...");
+                client = server.accept();
+
+                System.out.println("Connect request is accepted...");
+            }
+        }
+
+    }
+}
